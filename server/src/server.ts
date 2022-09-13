dd87cf91505bb1b0ebea5c17571d2b736d7bf188
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-09-13 18:29:41 -0300

    feat: first commit

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "John Doe",
+    },
+    {
+      id: 2,
+      name: "Jane Doe2",
+    },
+    {
+      id: 3,
+      name: "Jane Doe3",
+    },
+    {
+      id: 4,
+      name: "Jane Doe4",
+    },
+  ]);
+});
+
+app.listen(3333);
