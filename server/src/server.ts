5372536333035d7ddedd01fcf57dac46713d22e7
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-09-22 19:28:21 -0300

    feat: new modal ad

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.get("/games", (request, response) => {
-  return response.json([]);
-});
-
 app.post("/ads", (request, response) => {
   return response.status(201).json([]);
+});
+
+app.get("/games", (request, response) => {
+  return response.json([]);
 });
 
 app.get("/games/:id/ads", (request, response) => {
