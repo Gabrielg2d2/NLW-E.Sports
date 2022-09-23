d94cf8964262a927b1e904439e5a111288e3d792
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-09-23 17:24:32 -0300

    feat: route get discord

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
+
 const prisma = new PrismaClient({
   log: ["query", "info", "warn"],
 });
@@ -56,9 +57,18 @@ app.get("/games/:id/ads", async (request, response) => {
 });
 
 app.get("/games/:id/discord", async (request, response) => {
-  const gameId = request.params.id;
+  const adId = request.params.id;
 
-  return response.json([{ id: gameId, title: `Discord ${gameId}` }]);
+  const discord = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return response.json(discord);
 });
 
 app.listen(3333);
