a3efe7a6b97b88190efc3a54fe6e096fdd8e0844
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-09-23 16:06:56 -0300

    feat: request ads

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn'],
+  log: ["query", "info", "warn"],
 });
 
 app.post("/ads", (request, response) => {
@@ -24,13 +24,38 @@ app.get("/games", async (request, response) => {
   return response.json(games);
 });
 
-app.get("/games/:id/ads", (request, response) => {
+app.get("/games/:id/ads", async (request, response) => {
   const gameId = request.params.id;
 
-  return response.json([{ id: gameId, title: "Anuncio 1" }]);
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const adsFormatted = ads.map((ad) => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(","),
+    };
+  });
+
+  return response.json(adsFormatted);
 });
 
-app.get("/games/:id/discord", (request, response) => {
+app.get("/games/:id/discord", async (request, response) => {
   const gameId = request.params.id;
 
   return response.json([{ id: gameId, title: `Discord ${gameId}` }]);
